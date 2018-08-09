773aaa8576985154bbe4fc656648374545dc8cff
Anirban Biswas <anirban.biswas07@gmail.com>
2018-08-09 15:06:06 -0700

    removed loggin statements temprarily

--- ovsdb/jsonrpc-server.c
+++ ovsdb/jsonrpc-server.c
@@ -551,8 +551,8 @@ ovsdb_jsonrpc_session_run(struct ovsdb_jsonrpc_session *s)
 
         msg = jsonrpc_session_recv(s->js);
         if (msg) {
-            VLOG_INFO("method: %s from: %s\n", msg->method, s->js->remotes.names[0]);
-            VLOG_INFO("\nparams:\n%s\n", json_to_string(msg->params, JSSF_PRETTY));
+            // VLOG_INFO("method: %s from: %s\n", msg->method, s->js->remotes.names[0]);
+            // VLOG_INFO("\nparams:\n%s\n", json_to_string(msg->params, JSSF_PRETTY));
 
             if (msg->type == JSONRPC_REQUEST) {
                 ovsdb_jsonrpc_session_got_request(s, msg);
